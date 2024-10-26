1f2dd54f38bedb7647ec263f7397506a4fff5039
Serveresty <cs.go.12228@gmail.com>
2024-10-26 15:07:40 +0300

    removed

--- build/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GameShopPlatformClient_FILE E:/cppProj/GameShopPlatformClient/build/Release/GameShopPlatformClient.exe)
-set(__QT_DEPLOY_TARGET_GameShopPlatformClient_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GameShopPlatformClient_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
